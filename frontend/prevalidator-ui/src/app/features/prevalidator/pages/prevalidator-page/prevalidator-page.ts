efa588fae1e9db29c5efb386d3248385e9a2e024
Ingeniera Yessenia Tique Castro <cs00695@colserauto.com>
2026-02-05 05:10:09 -0500

    feat(prevalidator-ui): estructura y vista inicial del prevalidador

--- frontend/prevalidator-ui/src/app/features/prevalidator/pages/prevalidator-page/prevalidator-page.ts
+++ frontend/prevalidator-ui/src/app/features/prevalidator/pages/prevalidator-page/prevalidator-page.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+/* Angular Material */
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-prevalidator-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
   templateUrl: './prevalidator-page.html',
   styleUrls: ['./prevalidator-page.scss'],
 })
-export class PrevalidatorPageComponent {}
+export class PrevalidatorPageComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      fechaHoraInspeccion: [''],
+      fechaHoraSalidaInspeccion: [''],
+      tipo: [''],
+      codigoFasecolda: [''],
+      servicio: [''],
+      chasis: [''],
+      serial: [''],
+      motor: [''],
+      modelo: [''],
+      color: [''],
+      tipoCarroceria: [''],
+      tipoVehiculo: [''],
+      kilometraje: [''],
+      kilometrajePorAnio: [''],
+      tipoPintura: [''],
+      caja: [''],
+    });
+  }
+}
