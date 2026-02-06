3c1311a8561cc1e8789d646b0cc3a753591011ac
Ingeniera Yessenia Tique Castro <cs00695@colserauto.com>
2026-02-06 13:03:38 -0500

    feat(database): Se añade  repositorio de la vista api_hdi a la base de datos

---
+++ app/repositories/api_hdi_repository.py
@@ -0,0 +1,52 @@
+# app/repositories/api_hdi_repository.py
+
+from sqlalchemy import text
+from app.db.session import get_db
+
+class ApiHDIRepository:
+
+    @staticmethod
+    def get_by_placa_or_inspeccion(
+        placa: str | None = None,
+        id_service: int | None = None
+    ):
+        db = next(get_db())
+
+        query = """
+        SELECT
+            id_service,
+            service_type,
+            tipo,
+            servicio,
+            tipo_vehiculo,
+            initial_time,
+            final_time,
+            modelo,
+            numero_chasis,
+            numero_motor,
+            numero_serie,
+            color,
+            color_front,
+            color_id_hdi,
+            carroceria,
+            carroceria_id_hdi,
+            kilometraje,
+            tipo_caja,
+            id_caja_hdi,
+            codigo_fasecolda
+        FROM api_hdi
+        WHERE 1=1
+        """
+
+        params = {}
+
+        if placa:
+            query += " AND placa = :placa"
+            params["placa"] = placa
+
+        if id_service:
+            query += " AND id_service = :id_service"
+            params["id_service"] = id_service
+
+        result = db.execute(text(query), params).mappings().first()
+        return result
